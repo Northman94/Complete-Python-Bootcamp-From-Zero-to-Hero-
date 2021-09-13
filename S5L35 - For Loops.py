01b2d5016029bbbfa7a371cb2d82db482b8b951b
Northman94 <Zhandarm2000@gmail.com>
2021-09-13 23:36:48 +0300

    print("\n");

--- S5L35 - For Loops.py
+++ S5L35 - For Loops.py
@@ -1,3 +1,31 @@
+# Iterable =  obj can be iterated over. Go through every element in the Obj.
+# Through every letter in a String, every Obj is a List, every Key in a Dictionary…
+
+# Syntax of a "for" Loop:
+
+my_iterable = [1,2,3]
+
+for every_elmnt in my_iterable:
+    print(every_elmnt);
+
+print("\n");
+
+
+myList = [1,2,3,4,5]
+
+for numb in myList:
+    print(numb)
+
+print("\n");
+
+
+myList1 = [1,2,3]
+
+for i in myList:
+    print("hello")
+
+print("\n");
+
 
 myList2 = [1,2,3,4,5]
 
@@ -5,3 +33,4 @@ for numb2 in myList2:
     # Check for even numbs
     if numb2 % 2 == 0:
         print(numb2)
+
