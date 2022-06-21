8f2e49abf3e7741c71ecbe3e976350287a86debf
Northman94 <Zhandarm2000@gmail.com>
2022-06-21 22:53:01 +0300

    Update S0 - Random info.py

--- S0 - Random info.py
+++ S0 - Random info.py
@@ -1,7 +1,49 @@
 
 # Move Code Lines: Alt/option + Shift + Up/Down
 
+# Duplicate Code Line: Command + D
+
 
 import webbrowser as wb
 
 wb.open('https://www.humblebundle.com')
+
+# OR#
+
+#import webbrowser
+
+#webbrowser.open('https://www.google.com.ua')
+
+# = - = - = - = - = - = - = - = - = - = - = - = - =
+
+
+# One Line  IF-ELSE:
+
+var = 21 if 21>32 else 42
+print(var)
+# 42
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
